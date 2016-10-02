ddcfd8e9ce4e81e14c6825390dec7810e997073c
Guido Dechamps <Guido.Dechamps@solid-it.biz>
2016-10-02 14:21:52 +0200

    Added initial Acceptance test. Choose not to use Cucumber in favor of using junit 5

---
+++ src/test/java/be/solid/social/AcceptanceTests.java
@@ -0,0 +1,16 @@
+package be.solid.social;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@DisplayName("Social Kata Acceptance tests")
+public class AcceptanceTests {
+
+    @Test
+    @DisplayName("Publish own status")
+    void publishToPersonalTimeLine() {
+        assertTrue(false, "Not implemented yet");
+    }
+}
