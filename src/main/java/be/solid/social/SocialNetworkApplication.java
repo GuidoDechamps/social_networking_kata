b9d2b1df6b773aed6c0afdfc78c378f549a8fc8e
guido <guido.dechamps@solid-it.biz>
2017-09-09 10:41:08 +0200

    refactor parser

--- src/main/java/be/solid/social/SocialNetworkApplication.java
+++ src/main/java/be/solid/social/SocialNetworkApplication.java
@@ -1,9 +1,13 @@
-package be.solid.social.console;
+package be.solid.social;
+
+import be.solid.social.console.Command;
+import be.solid.social.console.CommandParser;
 
 import java.util.Optional;
 import java.util.Scanner;
 
-class SocialNetworkApplication {
+public class SocialNetworkApplication {
+
     public static void main(String[] args) {
         System.out.println("Starting SocialNetworkApplication");
         System.out.println("*************************");
@@ -11,14 +15,21 @@ class SocialNetworkApplication {
         while (true) {
             System.out.println("");
             System.out.println("[SNA] Please enter a command");
+
             final String line = scanner.nextLine();
-            Optional<Command> command = CommandParser.parseCommand(line);
-            System.out.println("[SNA] i read: " + line);
-            if(line.equalsIgnoreCase("exit"))
-                break;
+            if (line.equalsIgnoreCase("exit")) break;
+
+            processCommand(line);
+
         }
         System.out.println("*************************");
         System.out.println("Stopping SocialNetworkApplication");
+    }
+
+    private static void processCommand(String line) {
+        final Optional<Command> command = CommandParser.parseCommand(line);
+        System.out.println("[SNA] i read: " + line);
+        command.ifPresent(x -> System.out.println("[SNA] parsed to command: " + x.toString()));
     }
 
 
